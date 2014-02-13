bcfbca658c91c881c30ee97b9070aa5afb4ce9f6
Yacine Aitmaamar <yacine.aitmaamar@gmail.com>
2014-02-13 11:14:36 +0100

    Suppression du model Executer.java dans git

--- MyPendu/app/models/Executer.java
+++
@@ -1,9 +0,0 @@
-public class Executer
-{
-    public static void main(String[] args)
-    {
-        Pendu pendu = new Pendu(5) ;    // pendu autorisant 5 erreurs
-        System.out.println("A vous de jouer...") ;
-        pendu.jouer() ;                
-    }
-}

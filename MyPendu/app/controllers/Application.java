63ec6cbdeb17cd31c6d5a73bd6247a63159f3967
Yacine Aitmaamar <yacine.aitmaamar@gmail.com>
2014-02-13 11:13:37 +0100

    modification de la fonction Application().index()

--- MyPendu/app/controllers/Application.java
+++ MyPendu/app/controllers/Application.java
@@ -1,14 +1,33 @@
 package controllers;
-
+import models.*;
 import play.*;
 import play.mvc.*;
-
+import play.data.*;
+import static play.data.Form.*;
+import play.data.validation.Constraints.*;
 import views.html.*;
 
 public class Application extends Controller {
 
+	final static Mot mot = new Mot ("public/document/listmot.txt");
+    final static Pendu pendu = new Pendu (5);
+
+    public static class Lettre {
+        @Required public char lettre;
+        
+    } 
+
+
+
     public static Result index() {
-        return ok(index.render("Your new application is ready."));
+    	Form<Lettre> form = form(Lettre.class).bindFromRequest();
+    	Lettre data = form.get();
+        return ok(index.render(form, data.lettre,pendu, mot));
+            
+
     }
 
+
+
+   
 }
